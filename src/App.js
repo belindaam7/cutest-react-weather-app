79a57a5b5f5844430d6ef54063820c2810213f80
belindaam7 <bsamezquita@gmail.com>
2024-07-30 20:56:48 -0700

    Added footer and bootstrap dependency.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello from react weather app</h1>
+      <footer>
+        This project was coded by Belinda Amezquita and it is <a href="https://github.com/belindaam7/cutest-react-weather-app" target="_blank" rel="noreferrer">open source</a>.
+      </footer>
     </div>
   );
 }
